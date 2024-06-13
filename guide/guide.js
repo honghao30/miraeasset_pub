c6c816a3e5914582bad80e4f5533a4d10c358cfc
hongsh <hongsh@the-51.com>
2024-06-13 14:03:39 +0900

    가이드 작업중~

--- guide/guide.js
+++ guide/guide.js
@@ -43,20 +43,45 @@ window.addEventListener('load', function() {
         }
 
     }, 200); 
+    // 코드 미리보기
     const preElements = document.querySelectorAll('.code_view');
-
     preElements.forEach(pre => {
-        const lines = pre.innerText.split('\n');
+        // <와 >를 먼저 엔티티로 변환하여 HTML 태그를 텍스트로 변경
+        const content = pre.innerHTML
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+    
+        // 줄 단위로 쪼개서 앞뒤 공백 제거
+        const lines = content.split('\n').map(line => line);
         let formattedCode = '';
-
+    
+        // 변환된 내용을 한 줄씩 처리
         lines.forEach((line, index) => {
-            const escapedLine = line
-                .replace(/</g, '&lt;')
-                .replace(/>/g, '&gt;')
-                //.trim();
-            formattedCode += `<span class="code_line"><span class="line_number">${index + 1}</span>${escapedLine}</span>\n`;
+            formattedCode += `<span class="code_line"><span class="line_number">${index + 1}</span>${line}</span>\n`;
         });
-
         pre.innerHTML = formattedCode;
-    });       
+    });
+    // 반복 노가다 줄이기
+    const createrTable = (el, row, col) => { 
+        const table = document.querySelector('.' + el); 
+        const firstRow = table.querySelector('tr'); 
+
+        for (var i = 0; i < row; i++) {
+            const tr = document.createElement('tr');
+            for (var j = 0; j < col; j++) {
+                const td = document.createElement('td');
+                const numCell = firstRow.children[j];
+                if (numCell.classList.contains('num')) {
+                    // 'num' 클래스가 있는 경우 해당 셀의 내용을 1씩 증가시킴
+                    const numValue = parseInt(numCell.innerText);
+                    td.innerText = numValue + i; // 각 행마다 숫자를 증가시킴
+                } else {
+                    // 'num' 클래스가 없는 경우 첫 번째 행의 값을 그대로 사용
+                    td.innerText = numCell.innerText;
+                }
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }   
+    }
 });
