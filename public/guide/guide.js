a607bbe6696ff5ffa64f897cd3a8889555e6c46c
hongsh <hongsh@the-51.com>
2024-07-09 16:40:42 +0900

    파일 구조 변경~

--- public/guide/guide.js
+++ public/guide/guide.js
@@ -56,25 +56,39 @@ window.addEventListener('load', function() {
 
     }, 200); 
     // 코드 미리보기
-    const preElements = document.querySelectorAll('.code_view');
-    preElements.forEach(pre => {
-        // <와 >를 먼저 엔티티로 변환하여 HTML 태그를 텍스트로 변경
-        const content = pre.innerHTML
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;');
-    
-        // 줄 단위로 쪼개서 앞뒤 공백 제거
-        const lines = content.split('\n').map(line => line);
-        let formattedCode = '';
-    
-        // 변환된 내용을 한 줄씩 처리
-        lines.forEach((line, index) => {
-            formattedCode += `<span class="code_line"><span class="line_number">${index + 1}</span>${line}</span>\n`;
+    const convertCodeSamples = () => {
+        const codeSamples = document.querySelectorAll('.sample-code');
+        codeSamples.forEach(code => {
+            let originCode = code.innerHTML;
+            originCode = originCode.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+            console.log('기존', originCode);
+            const previewArea = code.nextElementSibling;
+            previewArea.innerHTML = originCode;
         });
-        pre.innerHTML = formattedCode;
-    });
+    };
+    
+    const formatCodePreviews = () => {
+        const preElements = document.querySelectorAll('.code_view');
+        preElements.forEach(pre => {
+            const content = pre.innerHTML
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;');
+            
+            const lines = content.split('\n').map(line => line);
+            let formattedCode = '';
+        
+            lines.forEach((line, index) => {
+                formattedCode += `<span class="code_line"><span class="line_number">${index + 1}</span>${line}</span>\n`;
+            });
+            pre.innerHTML = formattedCode;
+        });
+    };
+    
+    convertCodeSamples();
+    formatCodePreviews();
+
     // 반복 노가다 줄이기
-    const createrTable = (el, row, col) => { 
+    const createTable = (el, row, col) => { 
         const table = document.querySelector('.' + el); 
         const firstRow = table.querySelector('tr'); 
 
@@ -97,3 +111,24 @@ window.addEventListener('load', function() {
         }   
     }
 });
+
+// 공통영역 불러오기
+document.addEventListener("DOMContentLoaded", function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/guide/header.html', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // 기존 head 태그 제거
+            console.log("ㅇㅇㅇ")
+            var existingHead = document.getElementsByTagName('head')[0];
+            if (existingHead) {
+                existingHead.parentNode.removeChild(existingHead);
+            }
+            // 새로운 head 태그 추가
+            var headElement = document.createElement('head');
+            headElement.innerHTML = xhr.responseText;
+            document.documentElement.insertBefore(headElement, document.body);
+        }
+    };
+    xhr.send();
+});   
