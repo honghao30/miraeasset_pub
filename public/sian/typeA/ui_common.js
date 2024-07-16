f68d283152e35d5f8ea7342a1d660b303eb0e0bb
dahyun <dhnam@the-51.com>
2024-07-16 15:48:35 +0900

    시안 a 에 tab scroll 될 경우 : scrollIntoView
    시안 a 에 tab scroll 될 경우 : scrollIntoView

--- public/sian/typeA/ui_common.js
+++ public/sian/typeA/ui_common.js
@@ -1,56 +1,55 @@
 // 모달 열기
 const openModal = (event, type) => {
     const btn = event.target;
-    const modalId = btn.getAttribute('modal-id');
+    const modalId = btn.getAttribute("modal-id");
     const target = document.getElementById(modalId);
-    const wa = btn.getAttribute('aria-haspopup');
+    const wa = btn.getAttribute("aria-haspopup");
 
     if (btn) {
-        btn.classList.add('modal-open');
+        btn.classList.add("modal-open");
     }
 
-    if (target) {        
-        target.style.display = 'block';
+    if (target) {
+        target.style.display = "block";
 
         setTimeout(() => {
-            target.classList.add('is-active');                
-            document.body.classList.add('modal-open');
+            target.classList.add("is-active");
+            document.body.classList.add("modal-open");
         }, 300);
     }
     setTimeout(() => {
         if (wa) {
             tabEvent(btn, target);
-        }        
-    }, 300); 
+        }
+    }, 300);
 };
 
 // 모달 외부 클릭 이벤트 핸들러
-document.addEventListener("click", function(e) {    
-    if (e.target.classList.contains('modal__wrap--bg')) {
-        const activeModal = document.querySelector('.modal__wrap--bg.is-active');
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("modal__wrap--bg")) {
+        const activeModal = document.querySelector(".modal__wrap--bg.is-active");
         setTimeout(() => {
-            activeModal.classList.remove('is-active');
-            document.body.classList.remove('modal-open');     
-        }, 300);         
-        activeModal.style.display = 'none';
+            activeModal.classList.remove("is-active");
+            document.body.classList.remove("modal-open");
+        }, 300);
+        activeModal.style.display = "none";
     }
 });
 
 //모달창 닫기
 const closeModal = (event, openButton) => {
     const btn = event.currentTarget;
-    const activeModal = btn.closest('.modal__wrap--bg');
+    const activeModal = btn.closest(".modal__wrap--bg");
     if (activeModal) {
         if (openButton) {
-            openButton.classList.remove('modal-open');
+            openButton.classList.remove("modal-open");
         }
 
-        activeModal.classList.remove('is-active')        
-        document.body.classList.remove('modal-open');
-        
-        
+        activeModal.classList.remove("is-active");
+        document.body.classList.remove("modal-open");
+
         setTimeout(() => {
-            activeModal.style.display = 'none';
+            activeModal.style.display = "none";
             // 접근성 탭 이동 이벤트 호출
             if (openButton) {
                 tabEvent(openButton, activeModal);
@@ -61,113 +60,119 @@ const closeModal = (event, openButton) => {
 
 // 접근성 탭 이동
 const tabEvent = (btn, el) => {
-    const isModalOpen = el.classList.contains('is-active');
-    const modalWrap = el.querySelector('.modal__wrap');
-    
-    if (isModalOpen) {        
+    const isModalOpen = el.classList.contains("is-active");
+    const modalWrap = el.querySelector(".modal__wrap");
+
+    if (isModalOpen) {
         if (modalWrap) {
-            modalWrap.setAttribute('tabindex', '0');
+            modalWrap.setAttribute("tabindex", "0");
             modalWrap.focus();
         }
-        btn.setAttribute('aria-expanded', 'true');
-    } else {        
+        btn.setAttribute("aria-expanded", "true");
+    } else {
         if (modalWrap) {
-            modalWrap.setAttribute('tabindex', '');
+            modalWrap.setAttribute("tabindex", "");
         }
-        btn.setAttribute('aria-expanded', 'false');
+        btn.setAttribute("aria-expanded", "false");
         btn.focus();
     }
 };
 
 const addCloseModalListeners = (target, openButton) => {
-    const closeButtons = target.querySelectorAll('.close-modal');
-    closeButtons.forEach(button => {
-        button.addEventListener('click', (event) => closeModal(event, openButton));
+    const closeButtons = target.querySelectorAll(".close-modal");
+    closeButtons.forEach((button) => {
+        button.addEventListener("click", (event) => closeModal(event, openButton));
     });
 };
 
 // 탭메뉴
 function tabMenus(tabGroupSelector) {
     const tabGroup = document.querySelector(tabGroupSelector);
-    if (!tabGroup) return; 
+    if (!tabGroup) return;
 
-    const tabButtons = tabGroup.querySelectorAll('.tab-button-list li a');
-    const tabPanes = tabGroup.querySelectorAll('.tab-pane1');
+    const tabButtons = tabGroup.querySelectorAll(".tab-button-list li a");
+    const tabPanes = tabGroup.querySelectorAll(".tab-pane1");
 
-    if (!tabButtons.length || !tabPanes.length) return; 
+    if (!tabButtons.length || !tabPanes.length) return;
 
     tabButtons.forEach((button, index) => {
-        button.addEventListener('click', event => {
+        button.addEventListener("click", (event) => {
             event.preventDefault();
             handleTabClick(index);
+            button.parentElement.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
         });
     });
 
     function handleTabClick(index) {
-        tabButtons.forEach(btn => btn.parentElement.classList.remove('is-active'));
-        tabButtons[index].parentElement.classList.add('is-active');
-        tabPanes.forEach(pane => pane.classList.remove('is-active'));
-        tabPanes[index].classList.add('is-active');
+        tabButtons.forEach((btn) => btn.parentElement.classList.remove("is-active"));
+        tabButtons[index].parentElement.classList.add("is-active");
+        tabPanes.forEach((pane) => pane.classList.remove("is-active"));
+        tabPanes[index].classList.add("is-active");
     }
 }
 
 // UUID생성
 const generateUniqueId = () => {
-    return 'xxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return "xxxxxxxx".replace(/[xy]/g, function (c) {
+        var r = (Math.random() * 16) | 0,
+            v = c == "x" ? r : (r & 0x3) | 0x8;
         return v.toString(16);
     });
-}
+};
 
 // form 제어
 const checkLabel = () => {
-    const formElements = document.querySelectorAll('.form-element__wrap');
-    
-    formElements.forEach(el => {
+    const formElements = document.querySelectorAll(".form-element__wrap");
+
+    formElements.forEach((el) => {
         const uniqueId = generateUniqueId();
-        
+
         // Label 설정
-        const label = el.querySelector('label');
+        const label = el.querySelector("label");
         if (label) {
-            label.setAttribute('for', uniqueId);
+            label.setAttribute("for", uniqueId);
         }
-        
+
         // Input 설정
-        const inputItems = el.querySelectorAll('input');
+        const inputItems = el.querySelectorAll("input");
         if (inputItems.length > 0) {
             const firstInput = inputItems[0];
-            firstInput.setAttribute('id', uniqueId);
+            firstInput.setAttribute("id", uniqueId);
             const setTitle = label.textContent;
             inputItems.forEach((input, index) => {
-                input.setAttribute('title', setTitle + ' ' + (index + 1) + '번째 자리');
-            })
+                input.setAttribute("title", setTitle + " " + (index + 1) + "번째 자리");
+            });
         }
 
         //select box
-        const selectItems = el.querySelectorAll('select');
+        const selectItems = el.querySelectorAll("select");
         if (selectItems.length > 0) {
             const firstSelectItems = selectItems[0];
-            firstSelectItems.setAttribute('id', uniqueId);
+            firstSelectItems.setAttribute("id", uniqueId);
             const setTitle = label.textContent;
             selectItems.forEach((select, index) => {
-                select.setAttribute('title', setTitle + ' ' + (index + 1) + '선택하세요');
-            })
+                select.setAttribute("title", setTitle + " " + (index + 1) + "선택하세요");
+            });
         }
     });
-}
-checkLabel()
+};
+checkLabel();
 
 //숫자에 콤마
 const numComma = (num) => {
-    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
 // 숫자 애니메이션
 const animateNumbers = () => {
     const ANIMATION_SPEED = 100;
-    const numberCounts = document.querySelectorAll('.text-ani');
-    numberCounts.forEach(number => {
-        const num = parseInt(number.getAttribute('data-num'));
+    const numberCounts = document.querySelectorAll(".text-ani");
+    numberCounts.forEach((number) => {
+        const num = parseInt(number.getAttribute("data-num"));
         let count = 0;
         const increment = Math.ceil(num / 100);
 
@@ -192,86 +197,86 @@ const animateNumber = (element, count, increment, target) => {
 // dot pin
 const checkDotPin = (pw) => {
     const pwStr = pw.toString(); // 비밀번호를 문자열로 변환
-    const dotPinsFrame = document.querySelectorAll('.pin-dot');  
+    const dotPinsFrame = document.querySelectorAll(".pin-dot");
     let passWord = []; // 입력된 키 값을 저장할 배열
 
-    dotPinsFrame.forEach(frame => {
-        const dotPins = frame.querySelectorAll('.dot');
+    dotPinsFrame.forEach((frame) => {
+        const dotPins = frame.querySelectorAll(".dot");
         let currentIndex = 0; // 현재 입력된 키의 인덱스
 
-        document.addEventListener('keydown', (event) => {
+        document.addEventListener("keydown", (event) => {
             let keyValue = event.key;
 
             if (currentIndex < dotPins.length) {
-                dotPins[currentIndex].classList.add('is-active');
+                dotPins[currentIndex].classList.add("is-active");
                 passWord.push(keyValue); // 현재 키를 비밀번호 배열에 추가
                 currentIndex++;
 
                 // 4번째 키 입력이 끝나면 자동으로 검증
                 if (currentIndex === 4) {
-                    let inputPassword = passWord.join(''); // 입력된 비밀번호 문자열 생성
+                    let inputPassword = passWord.join(""); // 입력된 비밀번호 문자열 생성
                     let compare = pwStr === inputPassword;
 
                     if (compare) {
-                        console.log('비밀번호 일치');
+                        console.log("비밀번호 일치");
                     } else {
-                        console.log('비밀번호 불일치');
+                        console.log("비밀번호 불일치");
                     }
                 }
             }
 
             if (passWord.length > 4) {
-                console.log('You have entered 4 keys already.');
+                console.log("You have entered 4 keys already.");
                 return;
             }
         });
     });
-}
+};
 
-//dropdown menu  
-const dropdownMenus = document.querySelectorAll('.dropdown-menu__wrap');
+//dropdown menu
+const dropdownMenus = document.querySelectorAll(".dropdown-menu__wrap");
 
 function handleDropdownMenu(menu) {
-    const selectedOptionButton = menu.querySelector('.btn-dropdown');
-    const optionList = menu.querySelectorAll('.dropdown_list li button');
+    const selectedOptionButton = menu.querySelector(".btn-dropdown");
+    const optionList = menu.querySelectorAll(".dropdown_list li button");
 
-    function toggleMenu() {        
-        dropdownMenus.forEach(m => {
+    function toggleMenu() {
+        dropdownMenus.forEach((m) => {
             if (m !== menu) {
-                m.classList.remove('is-active');
-                m.querySelector('.btn-dropdown').classList.remove('is-active');
-                m.querySelector('.dropdown_list').classList.remove('is-active');
+                m.classList.remove("is-active");
+                m.querySelector(".btn-dropdown").classList.remove("is-active");
+                m.querySelector(".dropdown_list").classList.remove("is-active");
             }
-        });                        
-        menu.classList.toggle('is-active');
-        selectedOptionButton.classList.toggle('is-active');
-        selectedOptionButton.nextElementSibling.classList.toggle('is-active');
+        });
+        menu.classList.toggle("is-active");
+        selectedOptionButton.classList.toggle("is-active");
+        selectedOptionButton.nextElementSibling.classList.toggle("is-active");
     }
 
-    selectedOptionButton.addEventListener('click', toggleMenu);
-    optionList.forEach(option => {
-        option.addEventListener('click', () => {
-            const selectedValue = option.getAttribute('data-option');
+    selectedOptionButton.addEventListener("click", toggleMenu);
+    optionList.forEach((option) => {
+        option.addEventListener("click", () => {
+            const selectedValue = option.getAttribute("data-option");
             selectedOptionButton.textContent = selectedValue;
 
-            menu.querySelectorAll('.dropdown_list li').forEach(item => {
-                item.classList.remove('is-active');
+            menu.querySelectorAll(".dropdown_list li").forEach((item) => {
+                item.classList.remove("is-active");
             });
 
-            option.parentElement.classList.add('is-active');
+            option.parentElement.classList.add("is-active");
             toggleMenu();
         });
     });
-    document.addEventListener("click", function(e) {
-        if (menu.classList.contains('is-active') && !e.target.closest('.dropdown-menu__wrap')) {
-            menu.classList.remove('is-active');
-            selectedOptionButton.classList.remove('is-active');
-            selectedOptionButton.nextElementSibling.classList.remove('is-active');
+    document.addEventListener("click", function (e) {
+        if (menu.classList.contains("is-active") && !e.target.closest(".dropdown-menu__wrap")) {
+            menu.classList.remove("is-active");
+            selectedOptionButton.classList.remove("is-active");
+            selectedOptionButton.nextElementSibling.classList.remove("is-active");
         }
     });
 }
 
-dropdownMenus.forEach(handleDropdownMenu);  
+dropdownMenus.forEach(handleDropdownMenu);
 
 //스크롤 이벤트
 const ScrollEnterMain = () => {
@@ -290,13 +295,13 @@ const ScrollEnterMain = () => {
         scrollElements.forEach((el) => {
             if (elementInView(el, 1.25)) {
                 displayScrollElement(el);
-                el.classList.toggle('left-enter-effect', el.hasAttribute('left-enter'));
-                el.classList.toggle('right-enter-effect', el.hasAttribute('right-enter'));
-                el.classList.toggle('shadow-effect', el.hasAttribute('shadow-effect'));
-                el.classList.toggle('scroll-up', el.hasAttribute('scrollUp'));     
-                displayAnimation();                           
+                el.classList.toggle("left-enter-effect", el.hasAttribute("left-enter"));
+                el.classList.toggle("right-enter-effect", el.hasAttribute("right-enter"));
+                el.classList.toggle("shadow-effect", el.hasAttribute("shadow-effect"));
+                el.classList.toggle("scroll-up", el.hasAttribute("scrollUp"));
+                displayAnimation();
             } else if (elementOutofView(el)) {
-                hideScrollElement(el);                
+                hideScrollElement(el);
             }
         });
     };
@@ -332,13 +337,13 @@ const displayAnimation = () => {
 // 위 아래 구분을 위한 스크립트====================
 
 // const onDownScroll = () => {
-    // Additional downscroll logic
-    // This function will be called when scrolling down
-    // Add your additional downscroll actions here
+// Additional downscroll logic
+// This function will be called when scrolling down
+// Add your additional downscroll actions here
 // }
 
 // const onUpScroll = () => {
-    // Additional upscroll logic
-    // This function will be called when scrolling up
-    // Add your additional upscroll actions here
+// Additional upscroll logic
+// This function will be called when scrolling up
+// Add your additional upscroll actions here
 // }
