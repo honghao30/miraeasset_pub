37a14609321f7af9c141a219c9286fd943be1f33
hongsh <hongsh@the-51.com>
2024-07-17 10:53:28 +0900

    넷틀리아 파이

--- public/guide/guide.js
+++ public/guide/guide.js
@@ -73,7 +73,7 @@ window.addEventListener('load', function() {
         console.log("함수 실행");
         const location = window.location.href;
         const navList = document.querySelectorAll('.navgation__wrap--top li');        
-        console.log(location);
+        console.log('메인경로', location);
         if (location.includes('pub_list')) {
             console.log(location);
             navList[4].classList.add('is-active');
