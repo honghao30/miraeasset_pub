525385e559372278f7eacc72f6c1047324e9d52e
hongsh <hongsh@the-51.com>
2024-07-18 15:15:59 +0900

    js 정리~

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
-import bottomSheetHandle from '../js/ui_common.js';
+import { bottomSheetHandle, checkLabel, checkTextArea, numComma, focusNextInputOnMaxLength, checkInputFocus, tabMenus, handleDropdownMenu  } from '../js/ui_common.js';
+import { ScrollEnterMain } from '../js/scroll_event.js';
 
+// 퍼블 환경일 경우 settimeout을 지정.
 const checkPublishingFile = () => {
     const nowLocation = window.location.href;    
     if (nowLocation.includes('.html') || nowLocation.includes('https://miraeasse.netlify.app/')) {
@@ -12,6 +14,19 @@ const checkPublishingFile = () => {
 document.addEventListener('DOMContentLoaded', ()=> {
     const isPublishingEnvironment = checkPublishingFile();
     const executionTimer = isPublishingEnvironment ? 200 : 0;
+    const dropdownMenus = document.querySelectorAll('.dropdown-menu__wrap');
+
+    dropdownMenus.forEach(menu => {
+        handleDropdownMenu(menu);
+    });
+    checkLabel();
+    checkTextArea();
+    checkInputFocus();
+    focusNextInputOnMaxLength('.pin-code input');
+    tabMenus('.tab-content');
+    ScrollEnterMain();
+
+    // dom 로딩시간 체크 필요한 경우
     setTimeout(() => {
         bottomSheetHandle();
     }, executionTimer);
---
+++ assets/js/scroll_event.js
@@ -0,0 +1,68 @@
+
+// 스크롤 이벤트 처리 함수
+import {animateNumbers} from '../js/ui_common.js';
+
+let isAnimated = false;
+
+export const ScrollEnterMain = () => {
+    const scrollElements = document.querySelectorAll(".animation-enter");
+
+    if (!scrollElements) return;
+
+    const elementInView = (el, dividend = 1) => el.getBoundingClientRect().top <= (window.innerHeight || document.documentElement.clientHeight) / dividend;
+    const elementOutofView = (el) => el.getBoundingClientRect().top > (window.innerHeight || document.documentElement.clientHeight);
+    const displayScrollElement = (element) => element.classList.add("fade-in");
+    const hideScrollElement = (element) => {
+        element.classList.remove("fade-in", "left-enter-effect", "right-enter-effect", "shadow-effect", "scroll-up");
+    };
+
+    const handleScrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 1.25)) {
+                displayScrollElement(el);
+                el.classList.toggle('left-enter-effect', el.hasAttribute('left-enter'));
+                el.classList.toggle('right-enter-effect', el.hasAttribute('right-enter'));
+                el.classList.toggle('shadow-effect', el.hasAttribute('shadow-effect'));
+                el.classList.toggle('scroll-up', el.hasAttribute('scrollUp'));     
+                displayAnimation();                           
+            } else if (elementOutofView(el)) {
+                hideScrollElement(el);                
+            }
+        });
+    };
+
+    window.addEventListener("scroll", handleScrollAnimation);
+};
+
+export const displayAnimation = () => {        
+    if (!isAnimated) {
+        animateNumbers();
+        isAnimated = true;
+    }
+};
+
+// 위 아래 구분을 위한 스크립트
+let lastScrollTop = 0;
+const scrollEventManage = () => {
+const Yoffset = window.pageYOffset || document.documentElement.scrollTop;
+
+if (Yoffset > lastScrollTop) {
+    // down scroll code
+    console.log("scroll Down")
+    onDownScroll();
+} else {
+    console.log("scroll Up")
+    onUpScroll();
+}
+lastScrollTop = Yoffset <= 0 ? 0 : Yoffset;
+}
+window.addEventListener("scroll", scrollEventManage);
+// 위 아래 구분을 위한 스크립트====================
+
+const onDownScroll = () => {
+    
+}
+
+const onUpScroll = () => {
+
+}
