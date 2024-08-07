a470868eeb10077ab6e019b452e6685fff3c8781
dahyun <dhnam@the-51.com>
2024-08-07 13:24:32 +0900

    네틀리파이 원복
    네틀리파이 원복

--- assets/js/gnb_include.js
+++ assets/js/gnb_include.js
@@ -7,41 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         const navItems = headerNav.getElementsByTagName("li"); // typeof object
         const navItemsArray = Array.from(headerNav.getElementsByTagName("li")); // 배열로 순회
-        const pathName = window.location.pathname;
-
-        let currentPath = pathName.match(/[^/]+$/)[0];
-        // 파일 확장자 확인
-        const fileExtension = currentPath.split('.').pop().toLowerCase();
-
+        const currentPath = window.location.pathname;
         let activeIndex = -1;
 
         for (let i = 0; i < navItemsArray.length; i++) {
             // 현재 경로에 맞는 navItemsArray의 파일 찾기
             const link = navItemsArray[i].getElementsByTagName("a")[0];
             const href = link.getAttribute("href");
-
-            // 확장자가 'html'이 아닌 경우에만 변환
-            // if (fileExtension !== 'html') {
-            //     currentPath = currentPath.toUpperCase();
-            // }
-            console.log("경로2", currentPath, href);
-            // [Start] 네틀리파이에서 적용이 안돼서 파일명 지정함
-            if (currentPath === "dhc-co-0005-m") {
-                console.log(currentPath);
-                navItemsArray[0].classList.add("is-active");
-            } else if (currentPath === "dhc-ht-0000-m") {
-                console.log(currentPath);
-                navItemsArray[1].classList.add("is-active");
-                if (activeIndex !== -1) {
-                    const activeItem = navItemsArray.splice(activeIndex, 1)[0]; // 현재 페이지의 li.is-active
-                    navItemsArray.unshift(activeItem); // 현재 페이지의 li.is-active를 배열 맨앞에 추가하기
-
-                    // 기존 요소를 모두 제거하고 업데이트된 순서로 다시 추가
-                    headerNav.innerHTML = "";
-                    navItemsArray.forEach((item) => headerNav.appendChild(item));
-                }
-            }
-            // [End]네틀리파이에서 적용이 안돼서 파일명 지정함
 
             // 해당 url의 pathname이 NavTagHref을 포함하고 있다면 is-active 이중클래스 추가
             if (currentPath.includes(href)) {
@@ -75,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 const html = await response.text();
                 element.innerHTML = html;
 
-                // 로드가 완료된 후에 호출                
+                // 로드가 완료된 후에 호출
                 headerNavActive();
             } catch (error) {
                 console.error("Error loading include:", error);
